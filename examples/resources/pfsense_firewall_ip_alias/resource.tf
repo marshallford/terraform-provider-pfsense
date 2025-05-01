5386f6b70f8201c81cebfe5e803f50b79e595561
Marshall Ford <inbox@marshallford.me>
2025-04-30 19:52:43 -0500

    update pfsense_firewall_ip_alias examples to use ip attr (#47)
    Fixes #46

--- examples/resources/pfsense_firewall_ip_alias/resource.tf
+++ examples/resources/pfsense_firewall_ip_alias/resource.tf
@@ -3,9 +3,9 @@ resource "pfsense_firewall_ip_alias" "host_example" {
   name = "access_points"
   type = "host"
   entries = [
-    { address = "192.168.1.5", description = "hallway" },
-    { address = "192.168.1.6", description = "bedroom" },
-    { address = "192.168.1.7", description = "kitchen" },
+    { ip = "192.168.1.5", description = "hallway" },
+    { ip = "192.168.1.6", description = "bedroom" },
+    { ip = "192.168.1.7", description = "kitchen" },
   ]
 }
 
@@ -15,9 +15,9 @@ resource "pfsense_firewall_ip_alias" "network_example" {
   description = "virtual machines"
   type        = "network"
   entries = [
-    { address = "10.10.0.0/22" },
-    { address = "10.10.4.0/22" },
-    { address = "10.10.8.0/21" },
+    { ip = "10.10.0.0/22" },
+    { ip = "10.10.4.0/22" },
+    { ip = "10.10.8.0/21" },
   ]
 }
 
@@ -26,8 +26,8 @@ resource "pfsense_firewall_ip_alias" "advanced_example" {
   name = "poe"
   type = "host"
   entries = [
-    { address = pfsense_firewall_ip_alias.host_example.name },
-    { address = "192.168.1.10" },
-    { address = "ipcam01.lan" },
+    { ip = pfsense_firewall_ip_alias.host_example.name },
+    { ip = "192.168.1.10" },
+    { ip = "ipcam01.lan" },
   ]
 }
